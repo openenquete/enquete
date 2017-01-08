f550597585e0c98f4d2ae789d7fe3ad9b6950c6e
making <makingx@gmail.com>
2017-01-09 05:27:17 +0900

    Add bar charts

--- src/main/java/jjug/admin/ReportController.java
+++ src/main/java/jjug/admin/ReportController.java
@@ -1,14 +1,16 @@
 package jjug.admin;
 
 import java.io.IOException;
-import java.util.Locale;
-import java.util.Map;
-import java.util.UUID;
+import java.util.*;
+import java.util.stream.Stream;
 
+import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import jjug.questionnaire.enums.Difficulty;
 import jjug.questionnaire.enums.Satisfaction;
@@ -23,6 +25,8 @@ import lombok.RequiredArgsConstructor;
 public class ReportController {
 	private final SeminarRepository seminarRepository;
 	private final SeminarReportService seminarReportService;
+	private final ObjectMapper objectMapper;
+	private final MessageSource messageSource;
 
 	@GetMapping("admin/seminars/{seminarId}/report")
 	String report(@PathVariable UUID seminarId, Model model, Locale locale)
@@ -37,6 +41,52 @@ public class ReportController {
 		model.addAttribute("seminar", seminar);
 		model.addAttribute("satisfactionReport", satisfactionReport);
 		model.addAttribute("difficultyReport", difficultyReport);
+		model.addAttribute("satisfactionsArray",
+				arrayJsonString(satisfactionReport, Satisfaction.class, locale));
+		model.addAttribute("difficultiesArray",
+				arrayJsonString(difficultyReport, Difficulty.class, locale));
 		return "admin/report";
 	}
+
+	<T extends Comparable<T>> String arrayJsonString(
+			Map<Summary.Session, Summary.Report<T>> map, Class<? extends Enum> clazz,
+			Locale locale) throws IOException {
+		List<Object[]> dataList = new ArrayList<>();
+		int len = clazz.getEnumConstants().length;
+		dataList.add(labels(clazz, locale));
+		map.forEach((session, report) -> {
+			Object[] data = new Object[len + 1];
+			data[0] = session.getSessionName();
+			Map<Object, Double> dataMap = initMap(clazz);
+			long count = report.getCount();
+			report.getDetails().forEach(d -> {
+				dataMap.put(d.getValue(), (double) d.getCount() * 100 / count);
+			});
+			int i = 1;
+			Iterator<Double> iterator = dataMap.values().iterator();
+			while (iterator.hasNext() && i <= len) {
+				data[i++] = iterator.next();
+			}
+			dataList.add(data);
+		});
+		return objectMapper.writeValueAsString(dataList);
+	}
+
+	String[] labels(Class<? extends Enum> clazz, Locale locale) {
+		Enum[] values = clazz.getEnumConstants();
+		String[] labels = new String[values.length + 1];
+		labels[0] = "Session";
+		for (int i = 0; i < values.length; i++) {
+			labels[i + 1] = messageSource.getMessage(clazz.getSimpleName().toLowerCase()
+					+ "." + values[i].name().toLowerCase(), null, locale);
+		}
+		return labels;
+	}
+
+	<T extends Enum> Map<Object, Double> initMap(Class<T> clazz) {
+		Map<Object, Double> map = new LinkedHashMap<>();
+		Stream.of(clazz.getEnumConstants()).forEach(e -> map.put(e, 0.0));
+		return map;
+	}
+
 }
